0cd5f26f7873523a44eda684cace1ae40f233961
Massimo <massimo@mdipierro.local>
2013-05-06 14:18:55 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='nlib',
+      version='0.1',
+      description='nlib is a collection of numeric/scientific algorithms which do not required numpy',
+      author='Massimo DiPierro',
+      author_email='massimo.dipierro@gmail.com',
+      url='https://github.com/mdipierro/canvas',
+      install_requires=[],
+      py_modules=["nlib"],
+      license= 'BSD',
+      keywords='numerical',
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
